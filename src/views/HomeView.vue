382c05ab670a749bfefa3a890316c39ee69fab3c
ArchKeyboard734 <sbeijerling@gmail.com>
2025-04-15 14:59:22 +0200

    Replace project with new local content

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import test from '../components/test.vue'
+</script>
+
+<template>
+  <main>
+    <test />
+  </main>
+</template>
